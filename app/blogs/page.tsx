fcfcab3af8c9546f58e4e50c86e427c7d3691f54
nicholaschen09 <nicholas.chen243@gmail.com>
2025-03-30 20:26:57 -0400

    fixed blogs page width

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -38,7 +38,7 @@ const regularPosts = blogPosts;
 
 export default function BlogsPage() {
   return (
-    <div className="container mx-auto py-16 px-4 mb-50 md:px-0">
+    <div className="container mx-auto py-16 px-4 mb-50 md:px-0 max-w-6xl">
       <div className="mb-16">
         <h1 className="text-4xl font-bold mb-6 mt-20">Blogs</h1>
         <p className="text-lg text-zinc-400 max-w-3xl">
