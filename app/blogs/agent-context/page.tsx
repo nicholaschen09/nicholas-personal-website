19b178e5d868a0a5c62a20dc3764eb3c6efcff9f
nicholaschen09 <nicholas.chen243@gmail.com>
2026-06-17 00:22:35 -0400

    Fix agent skills demo spacing

--- app/blogs/agent-context/page.tsx
+++ app/blogs/agent-context/page.tsx
@@ -340,8 +340,7 @@ table team.agent_skill_asset
                   >
                     demo
                   </a>
-                  {' '}for agent skills
-                  !
+                  {' '}for agent skills!
                 </p>
                 <figure className="mt-6">
                   <img
